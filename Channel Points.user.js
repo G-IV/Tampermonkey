e232bf85b5b9fe9ac3523519cd195f8ce83d82c3
George Burrows <unccmarine@gmail.com>
2025-04-24 13:36:06 -0500

    added the script for tampermonkey, not sure what I'm doing yet

---
+++ Channel Points.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         BL3 & Twitch
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.twitch.tv/*
+// @match        https://*.ext-twitch.tv/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @run-at       document-idle
+// @grant        none
+// @require      file:///Users/georgeburrows/Documents/Desktop/Projects/BL3 & Twitch/BL3 & Twitch.user.js
+// ==/UserScript==
