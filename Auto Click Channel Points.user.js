b6bcdf00c41209b0e3fa779e82fa79485e68a5cd
G-IV <unccmarine@gmail.com>
2025-05-20 19:42:41 -0500

    added log for troubleshooting, fixed variable bug

--- Auto Click Channel Points.user.js
+++ Auto Click Channel Points.user.js
@@ -60,7 +60,9 @@ function searchForClaimBonusButton() {
         const claimBonusButton = document.querySelector('[aria-label="Claim Bonus"]');
         if (claimBonusButton) {
             console.log('Claim Bonus button found:', claimBonusButton);
-            monitorSpecificElement(querySelector.parentNode.parentNode.parentNode.parentNode);
+            parentElement = claimBonusButton.parentNode.parentNode.parentNode.parentNode;
+            console.log('Claim Bonus button clicked:', parentElement);
+            monitorSpecificElement(parentElement);
             clearInterval(intervalId); // Stop searching once the button is found and clicked
         } else {
             console.log('Claim Bonus button not found. Retrying in 5 seconds...');
