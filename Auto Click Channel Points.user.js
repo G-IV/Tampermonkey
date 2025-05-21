e9b9577be84b6afe794b3f476996700daeefacc3
G-IV <unccmarine@gmail.com>
2025-05-20 19:45:15 -0500

    fixed variable type bug

--- Auto Click Channel Points.user.js
+++ Auto Click Channel Points.user.js
@@ -60,7 +60,7 @@ function searchForClaimBonusButton() {
         const claimBonusButton = document.querySelector('[aria-label="Claim Bonus"]');
         if (claimBonusButton) {
             console.log('Claim Bonus button found:', claimBonusButton);
-            parentElement = claimBonusButton.parentNode.parentNode.parentNode.parentNode;
+            const parentElement = claimBonusButton.parentNode.parentNode.parentNode.parentNode;
             console.log('Claim Bonus button clicked:', parentElement);
             monitorSpecificElement(parentElement);
             clearInterval(intervalId); // Stop searching once the button is found and clicked
